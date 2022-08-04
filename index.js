fdbb94287abf2bc564cf97c9290d62debe0f39de
RoyOkite <royokite@gmail.com>
2022-08-04 13:46:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+// JS array lab assignment!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, "Broom"];
+}
+
+function prependCat(name) {
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, 2);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
